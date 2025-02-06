7566adab3bd796472376eb922253921294a8ab19
bruvmode <79219197+bruvmode@users.noreply.github.com>
2025-02-06 14:55:23 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,46 +1,50 @@
-input.onButtonPressed(Button.A, function () {
-    if (nextEvent == 1) {
-        nextEvent = randint(1, 5)
-        Timer = 0
-    }
+states.setEnterHandler("Event1", function () {
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        . # # # .
+        # . . . #
+        # . . . #
+        `)
 })
-let Timer = 0
-let nextEvent = 0
-nextEvent = 1
-Timer = 0
-basic.forever(function () {
-    while (nextEvent == 1) {
-        basic.showLeds(`
-            . . # . .
-            . # . # .
-            . # # # .
-            # . . . #
-            # . . . #
-            `)
-        for (let index = 0; index < 5; index++) {
-            basic.pause(1000)
-            Timer += 1
+states.addLoopHandler("Event1", function () {
+    while (!(input.buttonIsPressed(Button.A))) {
+        basic.pause(1000)
+        Timer += 1
+        if (Timer >= 5) {
+            states.setState("Death")
         }
     }
-    while (nextEvent != 1) {
-        basic.showLeds(`
-            . . . . #
-            . . . # .
-            # . # . .
-            . # . . .
-            . . . . .
-            `)
+    while (input.buttonIsPressed(Button.A)) {
+        states.setState("EventPick")
     }
 })
-basic.forever(function () {
-    while (Timer >= 5) {
-        basic.showLeds(`
-            . # . # .
-            # . # . #
-            # # # # #
-            # # # # #
-            . # . # .
-            `)
-        nextEvent = 100
+states.setEnterHandler("EventPick", function () {
+    nextEvent = randint(1, 5)
+    Timer = 0
+    if (nextEvent == 1) {
+    	
+    } else if (nextEvent == 2) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else {
+    	
     }
 })
+states.setEnterHandler("Death", function () {
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        # # # # #
+        # # # # #
+        . # . # .
+        `)
+})
+let nextEvent = 0
+let Timer = 0
+states.setState("EventPick")
