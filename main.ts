eb7e07e1b9a6fc883b0a99311607dfe91cf5d852
bruvmode <79219197+bruvmode@users.noreply.github.com>
2025-02-06 14:42:37 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
-let nextEvent = 1
+input.onButtonPressed(Button.A, function () {
+    if (nextEvent == 1) {
+        nextEvent = randint(1, 5)
+    }
+})
+let nextEvent = 0
+nextEvent = 1
 let Timer = 0
 basic.forever(function () {
     while (nextEvent == 1) {
@@ -11,6 +17,27 @@ basic.forever(function () {
             `)
         for (let index = 0; index < 5; index++) {
             basic.pause(1000)
+            Timer += 1
         }
+    }
+    while (nextEvent != 1) {
+        basic.showLeds(`
+            . . . . #
+            . . . # .
+            # . # . .
+            . # . . .
+            . . . . .
+            `)
+    }
+})
+basic.forever(function () {
+    while (Timer >= 5) {
+        basic.showLeds(`
+            . # . # .
+            # . # . #
+            # # # # #
+            # # # # #
+            . # . # .
+            `)
     }
 })
