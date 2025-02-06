88eca916437848fa70c364b4bafb1598db7939a7
bruvmode <79219197+bruvmode@users.noreply.github.com>
2025-02-06 14:27:00 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
+let nextEvent = 1
+let Timer = 0
 basic.forever(function () {
-	
+    while (nextEvent == 1) {
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            . # # # .
+            # . . . #
+            # . . . #
+            `)
+        for (let index = 0; index < 5; index++) {
+            Timer += 1
+        }
+    }
 })
